39321a1919aa64374ffc6c7a629c43a944aa4b6b
Filip Kober <koberfilip2@gmail.com>
2023-10-20 18:05:41 +0200

    first commit

---
+++ app/api/uczniowie/route.js
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+const mysql = require('mysql2/promise');
+
+export async function GET(request) {
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'dziennik'
+    })
+
+    const [rows] = await connection.execute("SELECT * FROM uczniowie");
+    return NextResponse.json(rows)
+}
