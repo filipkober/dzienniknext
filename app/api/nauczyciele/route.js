e542c1fe2b46a3549b4b969f689453a9b72765b3
Filip Kober <koberfilip2@gmail.com>
2023-11-22 23:23:44 +0100

    added routes for schools and teachers

---
+++ app/api/nauczyciele/route.js
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+const mysql = require('mysql2/promise');
+
+export async function GET(request) {
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'dziennik'
+    })
+
+    const [rows] = await connection.execute("SELECT * FROM nauczyciele");
+    connection.end();
+    return NextResponse.json(rows)
+}
